27736926997da4b83eb22a0f4620c2c538018a2f
unknown <zhaoshuaijiang@ZHAOSHUAIJIANG.sogou-inc.com>
2014-12-24 14:54:45 +0800

    extract f0 from raw

---
+++ main_extract_f0.m
@@ -0,0 +1,25 @@
+clear all;
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%author: zhaoshuaijiang
+%date: 20141223
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%Settings%%%%%%%%%%%%
+opts.F0frameUpdateInterval  = 5;   %frame shift(ms)
+opts.spectralUpdateInterval = 5;   %frame shift(ms)
+opts.F0searchLowerBound     = 40;  %F0 lower bound
+opts.F0searchUpperBound     = 800; %F0 upper bound
+fs   = 16000;
+RAW_PATH = './raw/';
+F0_PATH  = './f0/'; mkdir(F0_PATH);
+%%%%%%%%%%%%Settings%%%%%%%%%%%%
+
+fns_all = Utils_getFilenames(RAW_PATH, '.raw');
+for i = 1:length(fns_all)
+    fprintf('Extract the %d th file\n',i);
+    raw =  Utils_readFeature([RAW_PATH fns_all{i} '.raw'],'short',1);
+    [f0raw,ap]=exstraightsource(raw,fs,opts);
+    %sp = exstraightspec(raw,f0raw,fs);
+    save([F0_PATH fns_all{i} '.f0'],'-ascii','f0raw');
+end
+
