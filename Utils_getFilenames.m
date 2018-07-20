5bda1e62d87c8cc6cbd4c0731aca0efda0ed773c
shuaijiang <zhaoshuaijiang@didichuxing.com>
2018-07-20 16:26:00 +0800

    upload utils_getFilesnames

---
+++ Utils_getFilenames.m
@@ -0,0 +1,12 @@
+% shuaijiang@201807
+function filenames = Utils_getFilenames(path,lastname)
+temp = dir(path);
+cntr = 1;
+for i = 3:length(temp)  %ignore . and ..
+    tmp = temp(i).name;
+    [path,name,EXT] = fileparts(temp(i).name);
+    if (strcmp(EXT, lastname))
+        filenames{cntr} = tmp(1:end-length(lastname));
+        cntr = cntr + 1;
+    end
+end
